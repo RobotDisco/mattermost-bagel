23c46c02bfe672e438eb6aeb41fdd9cb790832bb
Gaelan D'costa <gdcosta@gmail.com>
2020-04-12 16:07:26 -0400

    Add function with create Config Struct from Environment Variables.

--- config/config.go
+++ config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"os"
+)
+
 type Config struct {
 	MattermostURL string
 	MattermostUser string
@@ -12,4 +16,20 @@ type Config struct {
 
 	SQLiteFile string
 	SQLiteStaleBoundary string
+}
+
+func CreateConfigFromEnvironmentVariables() Config {
+	return Config{
+		MattermostURL: os.Getenv("BAGEL_MATTERMOST_URL"),
+		MattermostUser: os.Getenv("BAGEL_USERNAME"),
+		MattermostPassword: os.Getenv("BAGEL_PASSWORD"),
+		MattermostTeam: os.Getenv("BAGEL_TEAM_NAME"),
+		MattermostChannel: os.Getenv("BAGEL_CHANNEL_NAME"),
+
+		DatabaseBackend: os.Getenv("BAGEL_DB_BACKEND"),
+		NonRepeatingPairsRetryAttempts: os.Getenv("BAGEL_NONREPEATING_PAIRS_MAX_RETRY_ATTEMPTS"),
+
+		SQLiteFile: os.Getenv("BAGEL_SQLITE_FILE"),
+		SQLiteStaleBoundary: os.Getenv("BAGEL_SQLITE_STALE_BOUNDARY"),
+	}
 }
